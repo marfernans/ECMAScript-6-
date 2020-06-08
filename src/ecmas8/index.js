c23a761c25343e407fd269508a336da1a7cbc10f
Mario Fernandez Serrano <me.fernandezserrano@gmail.com>
2020-06-07 21:33:37 -0500

    What was implemented in ES8?

---
+++ src/ecmas8/index.js
@@ -0,0 +1,35 @@
+// ¿Qué se implemento en ES8?
+
+const data = {
+  frontend: 'Mario Fernandez',
+  backent: 'Ronald Fernandez',
+  design: 'Miguelito Fernandez',
+}
+
+const entries = Object.entries(data);
+console.log (entries);
+console.log(entries.length);
+
+
+
+const data = {
+  frontend: 'Mario Fernandez',
+  backent: 'Ronald Fernandez',
+  design: 'Miguelito Fernandez',
+}
+
+const values = Object.values(data);
+console.log(values);
+console.log(values.length);
+
+
+const string = 'Hello';
+console.log(string.padStart(7, 'hi'));
+console.log(string.padEnd(8, 'bye'));
+console.log('food'.padEnd(8, '----'));
+
+
+const obj = {
+  name: ' Marius',
+} 
+
